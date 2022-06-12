872f159629db1e37499a7739927a129cbbccaba1
Matheus <matessousa@outlook.com>
2022-06-12 01:35:43 -0300

    chore: create feedback molecule

---
+++ src/components/molecules/Feedback.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+
+const icons = [
+    {
+        icon: 'i-ri-bug-fill',
+        iconName: 'Bug',
+    },
+    {
+        icon: 'i-ri-lightbulb-flash-fill',
+        iconName: 'Idea',
+    },
+    {
+        icon: 'i-ri-message-fill',
+        iconName: 'Others',
+    },
+]
+
+</script>
+
+<template>
+    <div flex flex-col items-center gap-10>
+        <div flex  items-center  justify-between>
+            <h1>Deixe seu feedback</h1>
+            <div i-ri-close-fill/>
+        </div>
+        <div flex justify-center items-center gap-5>
+            <div v-for="icon in icons" :key="icon.iconName" p-5 bg-dark-surface-primary>
+                <Icon  :icon="icon.icon" :iconName="icon.iconName" flex  flex-col items-center gap-2 />
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+
+<style scoped>
+</style>
