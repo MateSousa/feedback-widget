dd5d7d152c31d819591adaced5b643fdbfdf5004
Matheus <matessousa@outlook.com>
2022-06-12 01:35:27 -0300

    chore: create icon atom

---
+++ src/components/Atoms/Icon.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+
+defineProps({
+    icon: {
+        type: String,
+        default: '',
+    },
+    iconName: {
+        type: String,
+        default: '',
+    },
+})
+
+</script>
+
+<template>
+    <div>
+        <div :icon="icon" />
+        <p>{{iconName}}</p>
+    </div>
+</template>
+
