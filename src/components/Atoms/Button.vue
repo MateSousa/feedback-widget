f42b72cb4b60e7d6372958b240ba9187b2533122
Matheus <matessousa@outlook.com>
2022-06-12 01:35:12 -0300

    chore: create btn ato

---
+++ src/components/Atoms/Button.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+defineProps({
+    text: {
+        type: String,
+        default: '',
+    },
+    
+})
+</script>
+
+<template>
+    <button>
+        {{text}}
+    </button>
+</template>
+
+
+<style scoped>
+</style>
