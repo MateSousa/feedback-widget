4712106b49d7b4a3694b7bb146e4d09d4dfba09d
Matheus <matessousa@outlook.com>
2022-06-12 00:27:47 -0300

    chore:first commit

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,30 @@ export default defineConfig({
         presetUno(),
         presetIcons(),
       ],
+      theme: {
+        colors: {
+          brand: '#8257e5',
+          brandHover: '#996dff',
+          textOnBrandColor: '#fff',
+          darkSurfacePrimary: '#18181b',
+          darkSurfaceSecondary: '#2727a',
+          darkSecondaryHover: '#3f3f46',
+          darkStroke: '#52525b',
+          darkTooltip:  '#f4f4f5',
+          darkTextPrimary: '#f4f4f5',
+          darkTextSecondary: '#a1a1aa',
+          darkTextOnTooltip: '#27272a',
+          whiteSurfacePrimary: '#fff',
+          whiteSurfaceSecondary: '#f4f4f5',
+          whiteSecondaryHover: '#e4e4e7',
+          whiteStroke: '#d4d4d8',
+          whiteTooltip: '#27272a',
+          whiteTextPrimary: '#27272a',
+          whiteTextSecondary: '#711717a',
+          whiteTextOnTooltip: '#f4f4f5',
+
+        }
+      }
     }),
     Components(),
     AutoImport({
